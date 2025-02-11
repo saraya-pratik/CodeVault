7569da852a095cb88317e125b878660aa6187e30
Pratik Saraya <122872290+saraya-pratik@users.noreply.github.com>
2025-02-11 09:43:19 +0530

    Time: 1 ms (99.46%) | Memory: 42.6 MB (70.72%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        int n=part.length();
+        while(s.indexOf(part)!=-1){
+            int ind=s.indexOf(part);
+            s=s.substring(0,ind)+s.substring(ind+n,s.length());
+        }
+        return s;
+    }
+}

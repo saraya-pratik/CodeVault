458554e5863f2e2b9230ddda2a801441ef692350
Pratik Saraya <122872290+saraya-pratik@users.noreply.github.com>
2025-11-21 10:01:23 +0530

    Time: 176 ms (73.86%) | Memory: 46.2 MB (34.04%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,25 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int res=0;
-        int[] count=new int[123];
-        for(int i=0;i<s.length()-2;i++){
-            if(count[s.charAt(i)]!=0)
-                continue;
-            int[] countb=new int[123];
-            for(int j=i+1;j<s.length()-1;j++){
-                if(countb[s.charAt(j)]!=0)
-                    continue;
-                for(int k=j+1;k<s.length();k++)
-                    if(s.charAt(i)==s.charAt(k)){
-                        res++;
-                        break;
-                    }
-                countb[s.charAt(j)]=1;
+        int ans = 0; // will store total Palindromic subseq of length 3
+
+        // Try each character from 'a' to 'z' as the outer character of the palindrome: x _ x
+        for (char c = 'a'; c <= 'z'; c++) {
+            // Find the first and last occurrence of character c in the string
+            int L = s.indexOf(c);
+            int R = s.lastIndexOf(c);
+
+            // Valid palindrome x_y_x requires at least one character in between → R - L >= 2
+            if (L != -1 && R - L >= 2) {
+                // Collect all distinct middle characters between L and R
+                HashSet<Character> mid = new HashSet<>();
+                for (int i = L + 1; i < R; i++) {
+                    // Every distinct middle character forms a unique palindrome: c + midChar + c
+                    mid.add(s.charAt(i));
+                }
+                ans += mid.size(); // Number of distinct middle characters = number of unique palindromes for this outer character
             }
-            count[s.charAt(i)]=1;
         }
-        return res;
+
+        return ans;
     }
 }
